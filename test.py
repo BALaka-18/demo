75bbda071b6ee81c63bfb7da413046a870a468c8
BALaka-18 <balaka2605@gmail.com>
2020-09-01 10:43:30 +0530

    check 2

--- test.py
+++ test.py
@@ -1,6 +1,29 @@
-def printLine(line):
-    print(f'You wanted to print : {line}')
+s = 'aabbcadaabb'
+N = len(s)
+mid = N // 2
+start1, start2 = 0, 0
+end1, end2 = N - 1, N - 1
+length = 0
 
+if N % 2 == 0:
+    start2 = mid
+    while start1 < mid and start2 < N:
+        if s[start1] == s[start2]:
+            length = length + 1
+            start1 = start1 + 1
+            start2 = start2 + 1
+        else:
+            start2 = start2 + 1
+            continue
+else:
+    start2 = mid + 1
+    while start1 < mid - 1 and start2 < N:
+        if s[start1] == s[start2]:
+            length = length + 1
+            start1 = start1 + 1
+            start2 = start2 + 1
+        else:
+            start2 = start2 + 1
+            continue
 
-if __name__ == '__main__':
-    printLine('Hello world')
+print(length)
